b55e9ee0a981007ffd9da2280c27ab0f650564cf
Alfredo Navas <elpuas@gmail.com>
2026-05-07 06:30:26 -0600

    Implement functional dynamic product selector block

--- blocks/product-selector/index.js
+++ blocks/product-selector/index.js
@@ -1,16 +1,67 @@
-( function ( blocks, element, i18n ) {
+( function ( blocks, element, i18n, blockEditor, components ) {
 	const el = element.createElement;
+	const __ = i18n.__;
+	const useBlockProps = blockEditor.useBlockProps;
+	const InspectorControls = blockEditor.InspectorControls;
+	const PanelBody = components.PanelBody;
+	const TextControl = components.TextControl;
+	const DEFAULT_BUTTON_LABEL = __( 'Agregar a Cotización', 'epdc-product-selector' );
 
 	blocks.registerBlockType( 'epdc/product-selector', {
-		edit: function Edit() {
+		edit: function Edit( props ) {
+			const attributes = props.attributes;
+			const setAttributes = props.setAttributes;
+			const buttonLabel = attributes.buttonLabel || DEFAULT_BUTTON_LABEL;
+			const blockProps = useBlockProps( {
+				className: 'epdc-product-selector-editor',
+			} );
+
 			return el(
-				'p',
-				{ className: 'epdc-product-selector-editor-placeholder' },
-				i18n.__( 'EPDC Product Selector block scaffold.', 'epdc-product-selector' )
+				element.Fragment,
+				null,
+				el(
+					InspectorControls,
+					null,
+					el(
+						PanelBody,
+						{
+							title: __( 'Button', 'epdc-product-selector' ),
+							initialOpen: true,
+						},
+						el( TextControl, {
+							label: __( 'Button label', 'epdc-product-selector' ),
+							value: buttonLabel,
+							onChange: function onChange( value ) {
+								setAttributes( {
+									buttonLabel: value || DEFAULT_BUTTON_LABEL,
+								} );
+							},
+						} )
+					)
+				),
+				el(
+					'div',
+					blockProps,
+					el(
+						'button',
+						{
+							type: 'button',
+							className: 'epdc-product-selector-button',
+							disabled: true,
+						},
+						buttonLabel
+					)
+				)
 			);
 		},
 		save: function Save() {
 			return null;
 		},
 	} );
-} )( window.wp.blocks, window.wp.element, window.wp.i18n );
+} )(
+	window.wp.blocks,
+	window.wp.element,
+	window.wp.i18n,
+	window.wp.blockEditor,
+	window.wp.components
+);
